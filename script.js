53fa30d2c78f945f351a991680628f3ccffd4e10
Silas Dias <saosilas@hotmail.com>
2023-02-03 16:21:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+let count = 1;
+
+document.getElementById("radio" + count).checked = true;
+
+setInterval(function () {
+    changeImg();
+}, 5000)
+
+function changeImg() {
+
+
+    if (count > 4) {
+        count = 1;
+    }
+    document.getElementById("radio" + count).checked = true;
+    count++;
+
+
+} 
+var left = document.getElementById('left');
+left.addEventListener('click', backwardsImg);
+
+   
+    function backwardsImg() {
+        //debugger
+        if(count>1){
+            count -= 1;
+            document.getElementById("radio" + count).checked = true;
+        }
+        else{
+            count=5;
+        }
+       
+    }
+    
+    var left = document.getElementById('right');
+    left.addEventListener('click', forwardsImg);
+    function forwardsImg() {
+        //debugger
+        if(count<4){
+            count+=1;
+            document.getElementById("radio" + count).checked = true;
+        }
+        else{
+            count=0;
+        }
+    }
+
+
+
+function openNav() {
+    document.getElementById("menu-nav").style.display = 'block';
+    document.getElementById("btn-close").style.display = 'block';
+    document.getElementById("btn-open").style.display = 'none';
+    document.getElementById("menu-nav").style.marginLeft = '0%';
+}
+function closeNav() {
+    document.getElementById("menu-nav").style.display = 'none';
+    document.getElementById("btn-open").style.display = 'block';
+    document.getElementById("btn-close").style.display = 'none';
+    document.getElementById("menu-nav").style.marginLeft = '100%';
+}
+
+function openSubMenu() {
+
+    let sub = document.getElementById("openSubMenu").style.display;
+    if (sub == 'none') {
+
+        document.getElementById("openSubMenu").style.display = 'block';
+
+    }
+    else {
+        document.getElementById("openSubMenu").style.display = 'none';
+        count++;
+    }
+
+
+}
